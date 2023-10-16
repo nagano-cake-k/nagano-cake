d95696aef02af0a68ec4105a27019deb131df6ce
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-16 08:36:52 +0000

    addressesとadmin/ordersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'addresses/inidex'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
   namespace :public do
     get 'orders/new'
     get 'orders/thanks'
