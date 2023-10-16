b2e1179169575d2dfe0490173d3ab2ff28251d88
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-16 12:52:43 +0000

    サインイン、サインアウト後のルート記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+# before_action :authenticate_user!, except: [:about]
+#   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :telephone_number])
+  end
+
+  private
+
+  def after_sign_in_path_for(resource_or_scope)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
 end
