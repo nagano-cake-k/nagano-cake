b6b4ade086412318a66f55743618144114c3334e
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-10-17 02:47:44 +0000

    アソシエーション記述追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,13 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  has_many :cart_items, dependent: :destroy
+  # has_many :items, through: :cart_items (← customerでitemの情報を使いたいとき)
+  has_many :orders, dependent: :destroy
+  has_many :adresses, dependent: :destroy
+  
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  # has_many :items, through: :order_details (← orderでitemの情報を使いたいとき)
+  belongs_to :customer
 end
