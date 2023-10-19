9efbb8e9085e650ce7d420a0f3293ec1b3a30760
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-10-19 08:25:10 +0000

    customers/editのupdate

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -7,6 +7,23 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-    @customoer = current_customer
+    @customer = current_customer
   end
+  
+  def update
+    @customer = current_customer
+    @customer.update
+  end
+  
+  def withdraw
+    @customer = current_customer
+    # is_memberカラムをtrueに変更することで削除フラグを立てる
+    @customer.update(is_member: true)
+    if @customer.save
+      reset_session
+      flash[:notice] = "退会処理を実行しました"
+      redirect_to root_path
+    end
+  end
+  
 end
