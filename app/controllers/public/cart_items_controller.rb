591ce2a271b4537ff80773857a7c233d6729f99a
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-21 06:30:43 +0000

    コントローラーの記載

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,9 @@
 class Public::CartItemsController < ApplicationController
-  before_action :authenticate_menber!
+  before_action :authenticate_customer!
   
   def index
     @cart_items = CartItem.all
-    @total_payment = 0;
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
   
   def create
@@ -12,14 +12,13 @@ class Public::CartItemsController < ApplicationController
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       # 元々カート何あるもの[:item_id]
       # 今追加したparams[:cart_item][:item_id]
-      cart_item = current_customer.cart_items.find.by(item_id: params[:cart_item][:item_id])
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
-      redirect_to cart_items_pat
-    elsif
-      @cart_item.save
+      redirect_to public_cart_items_path
+    elsif @cart_item.save
       @cart_items = current_customer.cart_items.all
-      redirect_to cart_items_path
+      redirect_to public_cart_items_path
     else
       redirect_to item_path(item_id)
     end
@@ -29,25 +28,26 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     flash[:notice] = 'Cart item update successfully'
-    redirect_to cart_item_path
+    redirect_to public_cart_items_path
   end
 
   def destroy
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.destroy
-    @cart_items = CartItem.all
-    redirect_to cart_item_path
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to public_cart_item_path
     flash[:notice] = "Cart item was successfully destroyed."
   end
   
   def destroy_all
-    current_customer.cart_item.destroy_all
-    redirect_back(fallback_location: root_path)
+    cart_items = current_customer.cart_items.all
+    cart_items.destroy_all
+    flash[:notice] =  "Cart item was successfully all destroyed"
+    redirect_back fallback_location: root_path
   end
   
   private
-  def care_item_params
-    params.require(:cart_item).permit(:item_id, :amount, item_image)
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
   
 end
