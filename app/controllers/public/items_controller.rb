6f8eec0b47d35956f145f4c839f3f67347bc798d
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-21 06:30:59 +0000

    コントローラーの記載

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,13 +5,14 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @cart_item = CartItem
+    @cart_item = CartItem.new
+    # @into_cart = CartItem.new
   end
   
 private
 
  def item_params
-  params.require(:items).permit(:genre_id,:name,:description,:image_id,:price)
+  params.require(:items).permit(:genre_id,:name,:explanation,:image_id,:cost)
  end
 
 end
