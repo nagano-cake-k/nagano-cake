97b11d246c7b9c25035ead12f29dab64b8c34ea7
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-23 11:26:03 +0000

    orderのstatus記述

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,8 @@
 class OrderDetail < ApplicationRecord
+
+  belongs_to :order
+  belongs_to :item
+
+  enum production_status: {wait_payment: 0, confirm_payment: 1, maiking: 2, prepar_shipping: 3, finish_prepar: 4}
+
 end
