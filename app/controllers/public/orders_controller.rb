88602dc3bca91c82cfb32a3f7065db54b3154266
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-10-23 02:50:20 +0000

    注文確認画面制作中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,9 +12,20 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+
     @order = Order.new
-    @order.payment_method = params[:order][:payment_method].to_i
-    
+    @order.payment_method = params[:order][:payment_method]
+
+    @address_type = params[:order][:address_type]
+    case @address_type
+      when "ご自身の住所"
+        @selected_address = current_customer.post_code + "" + current_customer.address + "" + current_customer.last_name + "" + current_customer.first_name
+      when "登録済み住所から選択"
+        selected = Address.find(params[:order][:registered_address_id])
+        @selected_address = selected.post_code + " " + selected.address + " " + selected.name
+      when "新しいお届け先"
+        @selected_address = params[:order][:post_code] + "" + params[:order][:address] + "" + params[:order][:name]
+    end
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,12 +8,11 @@ class Order < ApplicationRecord
   }
 
   enum address_type: {
-    self_address: 0,
-    registered_address: 1,
-    new_address: 2
+    "ご自身の住所": 0,
+    "登録済み住所から選択": 1,
+    "新しいお届け先": 2
   }
 
-  enum payment_method: {"クレジットカード": 0,"銀行振込": 1}
 	enum status: {"入金待ち": 0,"入金確認": 1,"製作中": 2,"発送準備中": 3, "発送済み": 4}
 
 end
