b434ec7cc67f66c52c6057f902b64dc251b327a4
Kazuki-Igarashi <vki.0504@gmail.com>
2023-10-23 11:25:20 +0000

    コントローラー記述追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,29 @@
 class Admin::OrdersController < ApplicationController
-  def index
+before_action :authenticate_admin!
+  
+  def customer_index
+    @customer = Customer.find(params[:id])
+    @orders = @customer.orders.all
+  end 
+  
+  def show
+    @order = Order.find(params[:id])
+    @customer = Customer.find(params[:id])
+    @order_details = @order.order_details
   end
 
-  def show
+  def update
+  @order = Order.find(params[:id])
+  @order_details = OrderDetail.where(order_id: params[:id])
+  if @order.update(order_params)
+    @order_details.update_all(maiking_status: 1) if @order.status == "payment_confirmation"
+  end
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
