8881463394677c98d72ab93b7c98e7f005fd1089
yayu1303 <62428626+yauy1303@users.noreply.github.com>
2023-10-25 06:32:26 +0000

    注文関係enum修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -17,18 +17,18 @@ class Public::OrdersController < ApplicationController
 
     @address_type = params[:order][:address_type]
     case @address_type
-      when "ご自身の住所"
+      when "self_address"
         @selected_address = current_customer.post_code + " " + current_customer.address + " " + current_customer.last_name + " " + current_customer.first_name
         @order.post_code = current_customer.post_code
         @order.address = current_customer.address
         @order.name = current_customer.last_name + current_customer.first_name
-      when "登録済み住所から選択"
+      when "registered_address"
         @selected = Address.find(params[:order][:address_id])
         @selected_address = @selected.post_code + " " + @selected.address + " " + @selected.name
         @order.post_code = @selected.post_code
         @order.address = @selected.address
         @order.name = @selected.name
-      when "新しいお届け先"
+      when "new_address"
         @selected_address = params[:order][:post_code] + "" + params[:order][:address] + "" + params[:order][:name]
     end
   end
